a2940b25957dc89dcf6451927600c9c835948185
mina37 <minasherif7@hotmail.com>
2015-12-15 23:05:02 +0200

    perfectly working version

--- Forwarding/Forwarding.v
+++ Forwarding/Forwarding.v
@@ -29,32 +29,62 @@ module forwarding (IdExRs,IdExRt,ExMemWb,MemWbWb,ExMemRd,MemWbRd,FwdA,FwdB);
 	input		MemWbWb;////
 	input [4:0]	ExMemRd;
 	input [4:0]	MemWbRd;
+	input 		clk;
 	
 	output reg [1:0] FwdA;
 	output reg [1:0] FwdB;
-	always@(IdExRs,IdExRt)
-		begin
-			
-			if(ExMemWb==1 && ExMemRd != 5'b00000)
-				begin
-					if(ExMemRd == IdExRs)	FwdA <= 2'b10;
-				end
-			else if(MemWbWb == 1 && MemWbRd != 5'b00000)
-				begin
-					if (MemWbRd == IdExRs) FwdA <= 2'b01;
-				end
-			else FwdA <= 2'b00;
-			if(ExMemWb==1 && ExMemRd != 5'b00000)
-				begin
-					if(ExMemRd == IdExRt)  FwdB <=  2'b10;
-				end
-			else if(MemWbWb == 1 && MemWbRd != 5'b00000)
-				begin
-					if (MemWbRd == IdExRt) FwdB	<= 2'b01;
-				end
-			else FwdB <= 2'b00;	  
-		end
 	
+	
+	always @(IdExRs or IdExRt or ExMemWb or MemWbWb or ExMemRd or MemWbRd)//(posedge clk)//@(IdExRs[0] or IdExRs[1] or IdExRs[2] or IdExRs[3] or IdExRs[4] or IdExRt[0] or IdExRt[1] or IdExRt[2] or IdExRt[3] or IdExRt[4] or ExMemWb or MemWbWb or ExMemRd[0] or ExMemRd[1] or ExMemRd[2] or ExMemRd[3] or ExMemRd[4] or MemWbRd[0] or MemWbRd[1] or MemWbRd[2] or MemWbRd[3] or MemWbRd[4])//@(edge IdExRs or edge IdExRt or edge ExMemWb or edge MemWbWb or edge ExMemRd or edge MemWbRd) //IdExRs or IdExRt or ExMemWb or MemWbWb or ExMemRd or MemWbRd)
+		begin
+			FwdA=0;
+			if((ExMemWb==1) && (ExMemRd != 5'b00000))
+				begin
+					if(ExMemRd == IdExRs)
+						begin
+							FwdA = 2;
+						end
+					else if((MemWbWb == 1) && (MemWbRd != 5'b00000))
+						begin
+							if (MemWbRd == IdExRs)
+								begin
+									FwdA = 1;
+								end
+						end
+				end
+			
+				
+			else
+				begin
+					FwdA = 0;
+				end
+		end
+			
+		always @(IdExRs or IdExRt or ExMemWb or MemWbWb or ExMemRd or MemWbRd)//posedge clk)
+			begin
+				FwdB=0;
+			if((ExMemWb==1) && (ExMemRd != 5'b00000))
+				begin
+					if(ExMemRd == IdExRt)
+						begin
+							FwdB =  2;
+						end
+					else if((MemWbWb == 1) && (MemWbRd != 5'b00000))
+						begin
+							if (MemWbRd == IdExRt)
+								begin
+									FwdB = 1;
+								end
+						end
+				end
+			
+			else
+				begin
+					FwdB = 0;
+				end
+			end
+			
+		//assign FwdA = ((ExMemWb===1) && (ExMemRd !== 5'b00000))? ((ExMemRd === IdExRs)? 2'b10:(((MemWbWb == 1) && (MemWbRd !== 5'b00000))?
 	
 	//}} End of automatically maintained section
 	
