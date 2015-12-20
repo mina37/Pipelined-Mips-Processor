65de95113066b1d42c3d228ec2332a3ac3344ba9
michealmina2 <michealmina2@gmail.com>
2015-12-20 12:36:40 +0200

    Registers.v

---
+++ Registers.v
@@ -0,0 +1,35 @@
+module Registers(Read_Reg1,Read_Reg2,Write_Reg,Write_Dat,Read_Dat1,Read_Dat2,Reg_Write)
+
+input [4:0]
+Read_Reg1,
+Read_Reg2,
+Write_Reg,
+
+input [31:0]
+Write_Dat;
+
+input
+Reg_Write;
+
+output [31:0]
+Read_Dat1,
+Read_Dat2;
+
+reg [4:0]
+Read_Reg1,
+Read_Reg2,
+Write_Reg;
+
+reg [31:0]
+Write_Dat,
+Read_Dat1,
+Read_Dat2;
+
+wire
+Reg_Write;
+
+reg [31:0] Register_file [31:0];
+
+initial 
+	begin
+	
